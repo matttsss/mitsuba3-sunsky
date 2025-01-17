9fafb8355dd53a3e5f48ac1d21d944d6861103fa
Mattéo Santini <mattsantini2003@gmail.com>
2025-01-17 17:15:29 +0100

    Simplify test_sunsky.py make range_idx function

--- src/emitters/tests/test_sunsky.py
+++ src/emitters/tests/test_sunsky.py
@@ -1,6 +1,12 @@
 import pytest
+
+import numpy as np
+
 import drjit as dr
 import mitsuba as mi
+
+eps = 1e-3
+SUN_HALF_APERTURE_ANGLE = dr.deg2rad(0.5388/2.0)
 
 def make_emitter(turb, sun_phi, sun_theta, albedo, sky_scale, sun_scale):
     sp_sun, cp_sun = dr.sincos(sun_phi)
@@ -36,12 +42,9 @@ def eval_full_spec(plugin, si, wavelengths, render_res = (512, 1024)):
     return mi.TensorXf(output_image, (*render_res, nb_channels))
 
 
-def make_range_idx(nb, a, b):
-    return [(i/(nb-1)) * (b - a) + a for i in range(nb)]
-
-@pytest.mark.parametrize("turb", make_range_idx(7, 1, 10))
-@pytest.mark.parametrize("sun_eta", make_range_idx(5, 0, dr.pi/2))
-@pytest.mark.parametrize("albedo", make_range_idx(3, 0, 1))
+@pytest.mark.parametrize("turb",    np.linspace(1, 10, 7))
+@pytest.mark.parametrize("sun_eta", np.linspace(0, dr.pi/2, 5))
+@pytest.mark.parametrize("albedo",  np.linspace(0, 1, 3))
 def test01_sky_radiance(variants_vec_backends_once, turb, sun_eta, albedo):
     render_res = (1024//2, 1024)
 
@@ -71,6 +74,7 @@ def test01_sky_radiance(variants_vec_backends_once, turb, sun_eta, albedo):
     rendered_scene  = mi.TensorXf(dr.ravel(plugin.eval(si)), (*render_res, 3)) if mi.is_rgb \
                  else eval_full_spec(plugin, si, wavelengths, render_res)
 
+    # Load the reference image
     render_type = "rgb" if mi.is_rgb else "spec"
     ref_path = f"../renders/{render_type}/sky_{render_type}_eta{sun_eta:.3f}_t{turb:.3f}_a{albedo:.3f}.exr"
     reference_scene = mi.TensorXf(mi.Bitmap(ref_path))
@@ -78,19 +82,25 @@ def test01_sky_radiance(variants_vec_backends_once, turb, sun_eta, albedo):
     rtol = 0.005 if mi.is_rgb else 0.0354
     rel_err = dr.mean(dr.abs(rendered_scene - reference_scene) / (dr.abs(reference_scene) + 0.001))
 
-    assert rel_err <= rtol, f"Fail when rendering plugin: {plugin}\nMean relative error: {rel_err}, threshold: {rtol}"
+    assert rel_err <= rtol, (f"Fail when rendering plugin: {plugin}\n"
+                             f"Mean relative error: {rel_err}, threshold: {rtol}")
 
-
-@pytest.mark.parametrize("turb", make_range_idx(7, 1, 10))
-@pytest.mark.parametrize("eta_ray", make_range_idx(5, 1e-3, dr.pi/2 - 1e-3))
-@pytest.mark.parametrize("gamma", make_range_idx(4, 0, dr.deg2rad(0.5388/2.0) - 1e-3))
-def test02_sun_radiance(variants_vec_backends_once, turb, eta_ray, gamma):
+@pytest.mark.parametrize("turb",    np.linspace(1, 10, 7))
+@pytest.mark.parametrize("eta_ray", np.linspace(eps, dr.pi/2 - eps, 5))
+@pytest.mark.parametrize("gamma",   np.linspace(0, SUN_HALF_APERTURE_ANGLE - eps, 4))
+def test02_sun_radiance(variants_vec_spectral, turb, eta_ray, gamma):
     wavelengths = [320, 400, 500, 600, 700, 800]
     ref_rad = mi.Float([mi.hosek_sun_rad(turb, wav, eta_ray, gamma) for wav in wavelengths])
 
     phi = dr.pi/5
-    sun_theta = dr.pi/2 - eta_ray - gamma
-    sun_theta = dr.select(sun_theta < 0, dr.pi/2 - eta_ray + gamma, sun_theta)
+    theta_ray = dr.pi/2 - eta_ray
+
+    # Determine the sun's elevation based on the queried ray elevation
+    # There are two solutions to this problem, if the one with the - sign is less than 0, we take the other one
+    sun_theta = theta_ray - gamma
+    if sun_theta < 0:
+        sun_theta = theta_ray + gamma
+
     plugin = make_emitter(turb=turb,
                           sun_phi=phi,
                           sun_theta=sun_theta,
@@ -103,12 +113,12 @@ def test02_sun_radiance(variants_vec_backends_once, turb, eta_ray, gamma):
     si.wavelengths = mi.Float(wavelengths)
 
     sp, cp = dr.sincos(phi)
-    st, ct = dr.sincos(dr.pi/2 - eta_ray)
+    st, ct = dr.sincos(theta_ray)
     si.wi = -mi.Vector3f(cp * st, sp * st, ct)
 
     # Evaluate the plugin
     res = plugin.eval(si)[0]
-    rel_err = dr.abs(res - ref_rad) / (ref_rad + 1e-6)
+    rel_err = dr.mean(dr.abs(res - ref_rad) / (ref_rad + 1e-6))
 
     rtol = 1e-3
     assert rel_err <= rtol, (f"Fail when rendering sun with ray at elevation {eta_ray:.2f} and gamma {gamma:.2f}\n"
